9575863f58f1c1a046d122a86d1963ee3e98d49f
aliu <adam.liu@tw.synaptics.com>
2019-02-21 15:43:26 +0800

    use app.yaml to control PORT in GAE, we use 30000 as default to not occupied 8080 port.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = 30000;
+var port = process.env.PORT || 30000;
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
